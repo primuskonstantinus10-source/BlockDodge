8193fbaaad648cde51003250593506be8da2816f
primuskonstantinus10-source <primuskonstantinus10@gmail.com>
2026-03-21 17:19:29 +0200

    Add files via upload
    window opens ,background is there.everything is awesome!

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ import pygame
 import time
 import random
 
+pygame.init()
+
 WIDTH, HEIGHT = 800, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("spaceshot")
+
+BG = pygame.transform.scale(pygame.image.load("space1.jpg"), (WIDTH, HEIGHT))
+
+
+def draw():
+    WIN.blit(BG, (0, 0))
 
 
 def main():
@@ -15,4 +23,11 @@ def main():
                 run = False
                 break
 
+        draw()
+        pygame.display.update()
+
     pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
